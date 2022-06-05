28b92de877ea403297baac266e3cc5908b27fa5d
Pedro Evangelista <pedro.evangelista@gmail.com>
2022-06-05 11:24:17 -0300

    Improve tests for between-two-sets

--- src/test/hacker_rank/between_two_sets_test.clj
+++ src/test/hacker_rank/between_two_sets_test.clj
@@ -3,11 +3,22 @@
   (:require [hacker-rank.between-two-sets :refer [getTotalX]]))
 
 (deftest getTotalX-test
-  (is (= [4 8 16]
-         (getTotalX [2 4] [16 32 96])))
+  (testing "it returns empty list when there are no common factors"
+    (is (= []
+           (getTotalX [2] [3]))))
 
-  (is (= [6 12]
-         (getTotalX [2 6] [24 36])))
+  (testing "it returns common multiples on first that are factors of second"
+    (is (= [6]
+           (getTotalX [2 3] [6])))
 
-  (is (= [12 24]
-         (getTotalX [3 4] [24 48]))))
+    (is (= [2 4]
+           (getTotalX [2] [4])))
+
+    (is (= [4 8 16]
+           (getTotalX [2 4] [16 32 96])))
+
+    (is (= [6 12]
+           (getTotalX [2 6] [24 36])))
+
+    (is (= [12 24]
+           (getTotalX [3 4] [24 48])))))
