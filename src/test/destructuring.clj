4178df4f974ba309408d91532daed2b4ae2a7135
Pedro Evangelista <pedro.evangelista@gmail.com>
2022-06-06 09:19:38 -0300

    Complete map destructuring examples

---
+++ src/test/destructuring.clj
@@ -0,0 +1,57 @@
+(ns destructuring
+  (:require [clojure.test :refer :all]
+            [matcher-combinators.test :refer [match?]]
+            [matcher-combinators.matchers :as m]))
+
+(deftest map-destructuring-test
+  (testing "it destructures maps"
+    (testing "using entry [:keys [& symbols]]"
+      (let [{:keys [name]} {:name "Pedro"}]
+        (is (match? "Pedro" name))))
+
+    (testing "using entry [:namespace/keys [& symbol-names]]"
+      (let [{:person/keys [name]} {:person/name "Pedro"}]
+        (is (match? "Pedro" name))))
+
+    (testing "using entry [symbol :symbol]"
+      (let [{person-name :person/name} {:person/name "Pedro"}]
+        (is (match? "Pedro" person-name))))
+
+    (testing "mixing binding forms"
+      (let [{person-name :person/name
+             :person/keys [age]
+             :keys [label]} {:person/name "Pedro"
+                             :person/age 37
+                             :label :engineer}]
+        (is (= "Pedro" person-name))
+        (is (= 37 age))
+        (is (= :engineer label))))
+
+    (testing "nesting binding forms"
+      (let [{{:pet/keys [name]} :person/pet}
+            {:person/pet {:pet/name "Rex"}}]
+        (is (= "Rex" name))))
+
+    (testing "with binding to whole map using entry [:as symbol]"
+      (let [{:as person} {:person/name "Pedro"}]
+        (is (match? (m/equals {:person/name "Pedro"})
+                    person))))
+
+    (testing "with default values using entry [:or {symbol default-value}]"
+      (let [{:keys [name] :or {name "Unknown"}} {}]
+        (is (= "Unknown" name))))))
+
+(deftest destructuring-test
+  (testing "var binding occurs in"
+    (testing "let"
+      (let [a-symbol "a value"]
+        (is (= "a value" a-symbol))))
+
+    (testing "fn"
+      (is (= "a value"
+             ((fn [a-symbol] a-symbol) "a value"))))))
+
+(defmacro as-map [& vars]
+  (->> vars
+       (map (juxt keyword identity))
+       (into {})))
