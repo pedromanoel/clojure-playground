99e82c7ef4d0c471fae3d91ccd525c7ef727fa0c
Pedro Evangelista <pedro.evangelista@gmail.com>
2023-04-20 22:00:56 -0300

    Add macros.destructuring namespace

---
+++ src/main/pedromanoel/playground/macros/destructuring.clj
@@ -0,0 +1,1 @@
+(ns pedromanoel.playground.macros.destructuring)
---
+++ src/test/pedromanoel/playground/macros/destructuring_test.clj
@@ -0,0 +1,3 @@
+(ns pedromanoel.playground.macros.destructuring-test
+  (:require [clojure.test :refer :all]
+            [pedromanoel.playground.macros.destructuring :as destructuring]))
