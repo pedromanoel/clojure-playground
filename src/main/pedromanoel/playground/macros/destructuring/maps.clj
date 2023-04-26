0b667ce7caf3b0501daaa4f1212c0118193ef9dd
Pedro Evangelista <pedro.evangelista@gmail.com>
2023-04-25 22:53:19 -0300

    Refactor code for readability
    Extract each binding type to a function

--- src/main/pedromanoel/playground/macros/destructuring/maps.clj
+++ src/main/pedromanoel/playground/macros/destructuring/maps.clj
@@ -1,25 +1,41 @@
 (ns pedromanoel.playground.macros.destructuring.maps)
 
+(defn keys-bindings
+  [[_ spec-val] binding-val]
+  (->> spec-val
+       (map (juxt identity (comp (partial get binding-val) keyword)))
+       (reduce concat)))
+
+(defn as-bindings
+  [[_ spec-val] binding-val]
+  [spec-val binding-val])
+
+(defn or-bindings
+  [[_ spec-val]]
+  (->> spec-val
+       (map (juxt first (partial apply list 'or)))
+       (reduce concat)))
+
+(defn symbol-bindings
+  [[spec-key spec-val] binding-val]
+  [spec-key (->> spec-val keyword (get binding-val))])
+
 (defn bindings
   [[binding-spec binding-val]]
   (->> binding-spec
        (map (fn [[spec-key spec-val :as binding-spec]]
               (cond
                 (= :keys spec-key)
-                (->> spec-val
-                     (map (juxt identity (comp (partial get binding-val) keyword)))
-                     (reduce concat))
+                (keys-bindings binding-spec binding-val)
 
                 (= :as spec-key)
-                [spec-val binding-val]
+                (as-bindings binding-spec binding-val)
 
                 (= :or spec-key)
-                (->> spec-val
-                     (map (juxt first (partial apply list 'or)))
-                     (reduce concat))
+                (or-bindings binding-spec)
 
                 (symbol? spec-key)
-                [spec-key (->> spec-val keyword (get binding-val))]
+                (symbol-bindings binding-spec binding-val)
 
                 :else
                 binding-spec)))
