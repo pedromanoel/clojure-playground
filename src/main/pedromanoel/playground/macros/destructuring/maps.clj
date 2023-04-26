a769fa88f822b205af8663b39c44268b4ce38cc1
Pedro Evangelista <pedro.evangelista@gmail.com>
2023-04-25 23:17:51 -0300

    Add documentation

--- src/main/pedromanoel/playground/macros/destructuring/maps.clj
+++ src/main/pedromanoel/playground/macros/destructuring/maps.clj
@@ -1,25 +1,31 @@
 (ns pedromanoel.playground.macros.destructuring.maps)
 
-(defn keys-bindings
+(defn keys-spec->bindings
+  "Converts keys into bindings. Ex.: [{:keys [x]} {:x 1}] => [x 1]"
   [binding-val [spec-key spec-val]]
   (when (= :keys spec-key)
     (->> spec-val
              (map (juxt identity (comp (partial get binding-val) keyword)))
              (reduce concat))))
 
-(defn as-bindings
+(defn as-spec->bindings
+  "Converts as into bindings. Ex.: [{:as m} {:x 1}] => [m {:x 1}]"
   [binding-val [spec-key spec-val]]
   (when (= :as spec-key)
     [spec-val binding-val]))
 
-(defn or-bindings
+(defn or-spec->bindings
+  "Converts or into bindings. Ex.: [{:or {:x 0}} {:x 1}] => [x (or x 0)].
+
+  Note that this binding will fail if x was not previously destructured."
   [_ [spec-key spec-val]]
   (when (= :or spec-key)
     (->> spec-val
          (map (juxt first (partial apply list 'or)))
          (reduce concat))))
 
-(defn symbol-bindings
+(defn symbol-spec->bindings
+  "Converts symbols into bindings. Ex.: [{my-x :x} {:x 1}] => [my-x 1]"
   [binding-val [spec-key spec-val]]
   (when (symbol? spec-key)
     [spec-key (get binding-val (keyword spec-val))]))
@@ -27,9 +33,9 @@
 (defn bindings
   [[binding-spec binding-val]]
   (->> binding-spec
-       (map (some-fn (partial keys-bindings binding-val)
-                     (partial as-bindings binding-val)
-                     (partial or-bindings binding-val)
-                     (partial symbol-bindings binding-val)))
+       (map (some-fn (partial keys-spec->bindings binding-val)
+                     (partial as-spec->bindings binding-val)
+                     (partial or-spec->bindings binding-val)
+                     (partial symbol-spec->bindings binding-val)))
        (reduce concat)
        vec))
